d863451771d6e0527fcdfb5182c429d79ecddb9b
Thomas Schmitt <scdbackup@gmx.net>
2009-10-08 14:41:32 +0200

    Version leap to 0.6.24

--- libisofs/libisofs.h
+++ libisofs/libisofs.h
@@ -1090,7 +1090,7 @@ int iso_lib_is_compatible(int major, int minor, int micro);
  */
 #define iso_lib_header_version_major  0
 #define iso_lib_header_version_minor  6
-#define iso_lib_header_version_micro  23
+#define iso_lib_header_version_micro  24
 
 /**
  * Usage discussion:
