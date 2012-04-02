81dd6ce55a3f814879e3eaf4199d53b5a3bb30ea
Thomas Schmitt <scdbackup@gmx.net>
2012-04-02 19:12:39 +0200

    Version leap to 1.2.3

--- libisofs/libisofs.h
+++ libisofs/libisofs.h
@@ -84,7 +84,7 @@
  */
 #define iso_lib_header_version_major  1
 #define iso_lib_header_version_minor  2
-#define iso_lib_header_version_micro  2
+#define iso_lib_header_version_micro  3
 
 /**
  * Get version of the libisofs library at runtime.
