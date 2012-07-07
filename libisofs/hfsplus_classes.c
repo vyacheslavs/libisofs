b211ce68046d0e223968216853c8e06fee579bd8
Thomas Schmitt <scdbackup@gmx.net>
2012-07-07 11:40:18 +0200

    Clarified copyright of libisofs/hfsplus_classes.c

--- libisofs/hfsplus_classes.c
+++ libisofs/hfsplus_classes.c
@@ -1,4 +1,27 @@
 /*
+ * 
+ * Based on Unicode 3.2.0.
+ * See http://www.unicode.org/copyright.html
+ * Quote from there:
+ * "Copyright (c) 1991-2012 Unicode, Inc. All rights reserved.
+ * [...] 
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of the Unicode data files and any associated documentation
+ * (the "Data Files") or Unicode software and any associated documentation
+ * (the "Software") to deal in the Data Files or Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, and/or sell copies of the Data Files or Software, and to permit
+ * persons to whom the Data Files or Software are furnished to do so, provided
+ * that (a) the above copyright notice(s) and this permission notice appear
+ * with all copies of the Data Files or Software, (b) both the above copyright
+ * notice(s) and this permission notice appear in associated documentation,
+ * and (c) there is clear notice in each modified Data File or in the Software
+ * as well as in the documentation associated with the Data File(s) or
+ * Software that the data or software has been modified."
+ * 
+ *
+ * For this particular implementation:
+ *
  * Copyright (c) 2012 Vladimir Serbinenko
  * Copyright (c) 2012 Thomas Schmitt
  *
@@ -16,7 +39,6 @@
 
 #include "hfsplus.h"
 
-/* Based on Unicode 3.2.0.  */
 
 /* This encodes a matrix of page and character, with 16-bit words as elements.
    Initially the matrix is filled with zeros.
