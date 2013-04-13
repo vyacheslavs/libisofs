439a14da1d59753c448d039928432948bc80781e
Thomas Schmitt <scdbackup@gmx.net>
2013-04-13 12:11:26 +0200

    Bug fix: Reserved and unused fields of APM entries were not zeroed.

--- libisofs/system_area.c
+++ libisofs/system_area.c
@@ -880,7 +880,7 @@ static int iso_write_apm_entry(Ecma119Image *t, int apm_block_size,
     else
         block_fac = 2048 / apm_block_size;
         
-    memset(buf, apm_block_size, 0);
+    memset(buf, 0, apm_block_size);
     wpt = buf;
 
     /* Signature */
